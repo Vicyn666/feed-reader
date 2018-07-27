1ffe591afe55dfe40391bca9e27639c1adcd7169
Vitaly Fedorov <fedvit@yandex.ru>
2018-07-27 21:50:34 +0300

    minor changes

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -6,21 +6,14 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-        it('are defined and not null names', function () {
+        it('names are defined and not null', function () {
             for (let f = 0; f < allFeeds.length; f++) {
                 expect(allFeeds[f].name).toBeDefined();
                 expect(allFeeds[f].name.length).not.toBe(0);
             }
         });
 
-
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-
-         it('are defined and not null urls', function () {
+         it('urls are defined and not null', function () {
              for (let f = 0; f < allFeeds.length; f++) {
                  expect(allFeeds[f].url).toBeDefined();
                  expect(allFeeds[f].url.length).not.toBe(0);
@@ -28,15 +21,12 @@ $(function() {
          });
     });
 
-
-    /* TODO: Write a new test suite named "The menu" */
-
     describe('The menu', function() {
 
         it('is hidden by default', function () {
                 expect($('body').hasClass('menu-hidden')).toBe(true);
         });
-      
+
         it('changes visibility on click', function () {
                 $('.menu-icon-link').click();
                 expect($('body').hasClass('menu-hidden')).toBe(false);
@@ -46,9 +36,9 @@ $(function() {
         });
     });
 
-    
+
     describe('Initial Entries', function() {
-    
+
          beforeEach(function(done) {
             loadFeed(0, function() {
                 done();
@@ -59,8 +49,8 @@ $(function() {
             done();
          });
     });
-        
-        
+
+
     describe('New Feed Selection', function() {
          let initialFeed;
          beforeEach(function(done) {
